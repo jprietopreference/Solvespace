jprietopreference/Solvespace

commit 5ac78e770efcbf9f30421ab5857dcd1a6c4d0c54

Implement OS X port.

src/gtk/gtkmain.cpp
@@ -705,8 +705,12 @@ void PaintGraphics(void) {
     Glib::MainContext::get_default()->iteration(false);
 }
 
-void SetWindowTitle(const char *str) {
-    GW->set_title(str);
+void SetCurrentFilename(const char *filename) {
+    if(filename) {
+        GW->set_title(std::string("SolveSpace - ") + filename);
+    } else {
+        GW->set_title("SolveSpace - (not yet saved)");
+    }
 }
 
 void ToggleFullScreen(void) {
@@ -1218,16 +1222,18 @@ protected:
     }
 
     virtual bool on_motion_notify_event(GdkEventMotion *event) {
-        SS.TW.MouseEvent(false, event->state & Gdk::BUTTON1_MASK,
+        SS.TW.MouseEvent(/*leftClick*/ false,
+                         /*leftDown*/ event->state & Gdk::BUTTON1_MASK,
                          event->x, event->y);
 
         return true;
     }
 
     virtual bool on_button_press_event(GdkEventButton *event) {
-        SS.TW.MouseEvent(event->type == Gdk::BUTTON_PRESS,
-                         event->state & Gdk::BUTTON1_MASK,
-                         event->x, event->y);
+        if(!(event->state & Gdk::BUTTON1_MASK))
+            return false;
+
+        SS.TW.MouseEvent(/*leftClick*/ true, /*leftDown*/ true, event->x, event->y);
 
         return true;
     }
@@ -1424,15 +1430,15 @@ int main(int argc, char** argv) {
     TW->show_all();
     GW->show_all();
 
+    SS.Init();
+
     if(argc >= 2) {
         if(argc > 2) {
             std::cerr << "Only the first file passed on command line will be opened."
                       << std::endl;
         }
 
-        SS.Init(argv[1]);
-    } else {
-        SS.Init("");
+        SS.OpenFile(argv[1]);
     }
 
     main.run(*GW);

CMakeLists.txt
@@ -1,6 +1,7 @@
 # cmake configuration
 
 cmake_minimum_required(VERSION 2.8.12)
+cmake_policy(VERSION 3.0.0)
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH}
     "${CMAKE_SOURCE_DIR}/cmake/")
 
@@ -71,7 +72,10 @@ if(WIN32)
         "${CMAKE_SOURCE_DIR}/extlib/si")
     set(SPACEWARE_LIBRARIES
         "${CMAKE_SOURCE_DIR}/extlib/si/siapp.lib")
-else()
+elseif(APPLE)
+    find_package(PNG REQUIRED)
+    find_library(APPKIT_LIBRARY AppKit REQUIRED)
+else() # Linux and compatible systems
     find_package(SpaceWare)
 
     if(GUI STREQUAL "fltk")

cmake/MacOSXBundleInfo.plist.in
@@ -1,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>CFBundleDevelopmentRegion</key>
+        <string>English</string>
+        <key>CFBundleExecutable</key>
+        <string>solvespace</string>
+        <key>CFBundleInfoDictionaryVersion</key>
+        <string>6.0</string>
+        <key>CFBundleName</key>
+        <string>SolveSpace</string>
+        <key>CFBundlePackageType</key>
+        <string>APPL</string>
+        <key>CFBundleVersion</key>
+        <string>${solvespace_VERSION_MAJOR}.${solvespace_VERSION_MINOR}</string>
+        <key>CFBundleShortVersionString</key>
+        <string>${solvespace_VERSION_MAJOR}.${solvespace_VERSION_MINOR}</string>
+        <key>NSHumanReadableCopyright</key>
+        <string>© 2008-2015 Jonathan Westhues and other authors</string>
+        <key>NSPrincipalClass</key>
+        <string>NSApplication</string>
+        <key>NSMainNibFile</key>
+        <string>MainMenu</string>
+        <key>CFBundleIconFile</key>
+        <string>AppIcon</string>
+        <key>CFBundleDocumentTypes</key>
+        <array>
+                <dict>
+                        <key>CFBundleTypeExtensions</key>
+                        <array>
+                                <string>slvs</string>
+                        </array>
+                        <key>CFBundleTypeIconFile</key>
+                        <string>AppIcon.icns</string>
+                        <key>CFBundleTypeName</key>
+                        <string>SolveSpace sketch</string>
+                        <key>CFBundleTypeOSTypes</key>
+                        <array>
+                                <string>slvs</string>
+                        </array>
+                        <key>CFBundleTypeRole</key>
+                        <string>Editor</string>
+                </dict>
+        </array>
+</dict>
+</plist>

src/CMakeLists.txt
@@ -121,6 +121,27 @@ if(WIN32)
         win32/freeze.cpp
         win32/w32main.cpp
         win32/resource.rc)
+elseif(APPLE)
+    add_definitions(
+        -mmacosx-version-min=10.6
+        -fobjc-arc)
+
+    set(platform_SOURCES
+        cocoa/cocoamain.mm
+        unix/gloffscreen.cpp)
+
+    set(platform_XIBS
+        cocoa/MainMenu.xib
+        cocoa/SaveFormatAccessory.xib)
+
+    set(platform_ICONS
+        cocoa/AppIcon.iconset)
+
+    set(platform_RESOURCES
+        unix/solvespace-48x48.png)
+
+    set(platform_LIBRARIES
+        ${APPKIT_LIBRARY})
 elseif(HAVE_FLTK)
     include_directories(${FLTK_INCLUDE_DIR})
 
@@ -159,6 +180,52 @@ elseif(HAVE_GTK)
         ${FONTCONFIG_LIBRARIES}
         ${GLEW_LIBRARIES})
 endif()
+
+set(platform_BUNDLED_RESOURCES)
+
+foreach(xib ${platform_XIBS})
+    get_filename_component(nib ${xib} NAME_WE)
+    set(source ${CMAKE_CURRENT_SOURCE_DIR}/${xib})
+    set(target ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources/${nib}.nib)
+    list(APPEND platform_BUNDLED_RESOURCES ${target})
+
+    add_custom_command(
+        OUTPUT ${target}
+        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources
+        COMMAND ibtool --errors --warnings --notices
+            --output-format human-readable-text --compile
+            ${target} ${source}
+        COMMENT "Building Interface Builder file ${xib}"
+        DEPENDS ${xib})
+endforeach()
+
+foreach(icon ${platform_ICONS})
+    get_filename_component(name ${icon} NAME_WE)
+    set(source ${CMAKE_CURRENT_SOURCE_DIR}/${icon})
+    set(target ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources/${name}.icns)
+    list(APPEND platform_BUNDLED_RESOURCES ${target})
+
+    add_custom_command(
+        OUTPUT ${target}
+        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources
+        COMMAND iconutil -c icns -o ${target} ${source}
+        COMMENT "Building icon set ${icon}"
+        DEPENDS ${source})
+endforeach()
+
+foreach(res ${platform_RESOURCES})
+    get_filename_component(name ${res} NAME)
+    set(source ${CMAKE_CURRENT_SOURCE_DIR}/${res})
+    set(target ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources/${name})
+    list(APPEND platform_BUNDLED_RESOURCES ${target})
+
+    add_custom_command(
+        OUTPUT ${target}
+        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/solvespace.app/Contents/Resources
+        COMMAND ${CMAKE_COMMAND} -E copy ${source} ${target}
+        COMMENT "Copying resource file ${res}"
+        DEPENDS ${res})
+endforeach()
 
 # solvespace executable
 
@@ -218,12 +285,13 @@ set(solvespace_SOURCES
     srf/surfinter.cpp
     srf/triangulate.cpp)
 
-add_executable(solvespace WIN32
+add_executable(solvespace WIN32 MACOSX_BUNDLE
     ${libslvs_HEADERS}
     ${libslvs_SOURCES}
     ${util_SOURCES}
     ${platform_HEADERS}
     ${platform_SOURCES}
+    ${platform_BUNDLED_RESOURCES}
     ${generated_HEADERS}
     ${solvespace_HEADERS}
     ${solvespace_SOURCES})
@@ -243,7 +311,8 @@ if(SPACEWARE_FOUND)
 endif()
 
 install(TARGETS solvespace
-    RUNTIME DESTINATION bin)
+    RUNTIME DESTINATION bin
+    BUNDLE DESTINATION .)
 
 install(FILES unix/solvespace.desktop
         DESTINATION share/applications)

src/cocoa/AppIcon.iconset/icon_16x16.png
@@ -1,0 +1,1 @@
+../../unix/solvespace-16x16.png

src/cocoa/AppIcon.iconset/icon_32x32.png
@@ -1,0 +1,1 @@
+../../unix/solvespace-32x32.png

src/cocoa/MainMenu.xib
@@ -1,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="3.0" toolsVersion="5056" systemVersion="12F45" targetRuntime="MacOSX.Cocoa" propertyAccessControl="none" useAutolayout="YES">
+    <dependencies>
+        <plugIn identifier="com.apple.InterfaceBuilder.CocoaPlugin" version="5056"/>
+    </dependencies>
+    <objects>
+        <customObject id="-1" userLabel="First Responder" customClass="FirstResponder"/>
+        <customObject id="-2" userLabel="File's Owner" customClass="NSApplication"/>
+        <customObject id="-3" userLabel="Application"/>
+        <customObject id="-4" userLabel="Application Delegate" customClass="ApplicationDelegate"/>
+        <customObject id="YLy-65-1bz" customClass="NSFontManager"/>
+        <menu title="Main Menu" systemMenu="main" id="AYu-sK-qS6">
+            <items>
+                <menuItem title="SolveSpace" id="1Xt-HY-uBw">
+                    <modifierMask key="keyEquivalentModifierMask"/>
+                    <menu key="submenu" title="SolveSpace" systemMenu="apple" id="uQy-DD-JDr">
+                        <items>
+                            <menuItem title="About SolveSpace" id="5kV-Vb-QxS">
+                                <modifierMask key="keyEquivalentModifierMask"/>
+                                <connections>
+                                    <action selector="orderFrontStandardAboutPanel:" target="-1" id="Exp-CZ-Vem"/>
+                                </connections>
+                            </menuItem>
+                            <menuItem isSeparatorItem="YES" id="VOq-y0-SEH"/>
+                            <menuItem title="Preferences…" keyEquivalent="," id="BOF-NM-1cW">
+                                <connections>
+                                    <action selector="preferences:" target="-4" id="xyv-2f-7kO"/>
+                                </connections>
+                            </menuItem>
+                            <menuItem isSeparatorItem="YES" id="wFC-TO-SCJ"/>
+                            <menuItem title="Services" id="NMo-om-nkz">
+                                <modifierMask key="keyEquivalentModifierMask"/>
+                                <menu key="submenu" title="Services" systemMenu="services" id="hz9-B4-Xy5"/>
+                            </menuItem>
+                            <menuItem isSeparatorItem="YES" id="4je-JR-u6R"/>
+                            <menuItem title="Hide SolveSpace" keyEquivalent="h" id="Olw-nP-bQN">
+                                <connections>
+                                    <action selector="hide:" target="-1" id="PnN-Uc-m68"/>
+                                </connections>
+                            </menuItem>
+                            <menuItem title="Hide Others" keyEquivalent="h" id="Vdr-fp-XzO">
+                                <modifierMask key="keyEquivalentModifierMask" option="YES" command="YES"/>
+                                <connections>
+                                    <action selector="hideOtherApplications:" target="-1" id="VT4-aY-XCT"/>
+                                </connections>
+                            </menuItem>
+                            <menuItem title="Show All" id="Kd2-mp-pUS">
+                                <modifierMask key="keyEquivalentModifierMask"/>
+                                <connections>
+                                    <action selector="unhideAllApplications:" target="-1" id="Dhg-Le-xox"/>
+                                </connections>
+                            </menuItem>
+                            <menuItem isSeparatorItem="YES" id="kCx-OE-vgT"/>
+                            <menuItem title="Quit SolveSpace" keyEquivalent="q" id="4sb-4s-VLi">
+                                <connections>
+                                    <action selector="terminate:" target="-3" id="Te7-pn-YzF"/>
+                                </connections>
+                            </menuItem>
+                        </items>
+                    </menu>
+                </menuItem>
+            </items>
+        </menu>
+    </objects>
+</document>

src/cocoa/SaveFormatAccessory.xib
@@ -1,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="3.0" toolsVersion="5056" systemVersion="12F45" targetRuntime="MacOSX.Cocoa" propertyAccessControl="none" useAutolayout="YES">
+    <dependencies>
+        <plugIn identifier="com.apple.InterfaceBuilder.CocoaPlugin" version="5056"/>
+        <capability name="Alignment constraints with different attributes" minToolsVersion="5.1"/>
+    </dependencies>
+    <objects>
+        <customObject id="-2" userLabel="File's Owner" customClass="NSViewController">
+            <connections>
+                <outlet property="button" destination="nNy-fR-AhK" id="w3z-a4-Khs"/>
+                <outlet property="view" destination="c22-O7-iKe" id="w2z-a4-Khs"/>
+            </connections>
+        </customObject>
+        <customObject id="-1" userLabel="First Responder" customClass="FirstResponder"/>
+        <customObject id="-3" userLabel="Application"/>
+        <customView id="c22-O7-iKe">
+            <rect key="frame" x="0.0" y="0.0" width="294" height="51"/>
+            <subviews>
+                <popUpButton verticalHuggingPriority="750" translatesAutoresizingMaskIntoConstraints="NO" id="nNy-fR-AhK">
+                    <rect key="frame" x="105" y="12" width="34" height="26"/>
+                    <popUpButtonCell key="cell" type="push" bezelStyle="rounded" alignment="left" lineBreakMode="truncatingTail" borderStyle="borderAndBezel" imageScaling="proportionallyDown" inset="2" id="f8z-Qp-Igm">
+                        <behavior key="behavior" lightByBackground="YES" lightByGray="YES"/>
+                        <font key="font" metaFont="menu"/>
+                        <menu key="menu" title="OtherViews" id="VgN-HZ-Q4a"/>
+                    </popUpButtonCell>
+                    <connections>
+                        <binding destination="-2" name="selectedIndex" keyPath="index" id="AXx-uh-tee"/>
+                    </connections>
+                </popUpButton>
+                <textField horizontalHuggingPriority="251" verticalHuggingPriority="750" translatesAutoresizingMaskIntoConstraints="NO" id="z9Z-cA-QIW">
+                    <rect key="frame" x="17" y="17" width="73" height="17"/>
+                    <textFieldCell key="cell" scrollable="YES" lineBreakMode="clipping" sendsActionOnEndEditing="YES" title="File Format:" id="2vD-ht-BhF">
+                        <font key="font" metaFont="system"/>
+                        <color key="textColor" name="controlTextColor" catalog="System" colorSpace="catalog"/>
+                        <color key="backgroundColor" name="controlColor" catalog="System" colorSpace="catalog"/>
+                    </textFieldCell>
+                </textField>
+            </subviews>
+            <constraints>
+                <constraint firstItem="nNy-fR-AhK" firstAttribute="top" secondItem="c22-O7-iKe" secondAttribute="top" constant="15" id="B3P-9V-lvu"/>
+                <constraint firstItem="nNy-fR-AhK" firstAttribute="leading" secondItem="z9Z-cA-QIW" secondAttribute="trailing" constant="20" id="Uex-s1-7UF"/>
+                <constraint firstAttribute="bottom" secondItem="nNy-fR-AhK" secondAttribute="bottom" constant="15" id="pPM-eQ-gOD"/>
+                <constraint firstAttribute="trailing" relation="greaterThanOrEqual" secondItem="nNy-fR-AhK" secondAttribute="trailing" constant="20" id="rPT-bl-Md7"/>
+                <constraint firstItem="z9Z-cA-QIW" firstAttribute="baseline" secondItem="nNy-fR-AhK" secondAttribute="baseline" constant="1" id="spD-eU-Frq"/>
+                <constraint firstItem="nNy-fR-AhK" firstAttribute="leading" secondItem="c22-O7-iKe" secondAttribute="centerX" constant="-40" id="whX-1w-qMB"/>
+            </constraints>
+        </customView>
+        <userDefaultsController representsSharedInstance="YES" id="ybp-e7-hms"/>
+    </objects>
+</document>

commit 90b7cd8b4b4e3a75ce013ac64866fbfdc0f359f6

GTK: Fix bug introduced in 5ac78e7.

src/gtk/gtkmain.cpp
@@ -1230,10 +1230,9 @@ protected:
     }
 
     virtual bool on_button_press_event(GdkEventButton *event) {
-        if(!(event->state & Gdk::BUTTON1_MASK))
-            return false;
-
-        SS.TW.MouseEvent(/*leftClick*/ true, /*leftDown*/ true, event->x, event->y);
+        SS.TW.MouseEvent(/*leftClick*/ event->type == Gdk::BUTTON_PRESS,
+                         /*leftDown*/ event->state & Gdk::BUTTON1_MASK,
+                         event->x, event->y);
 
         return true;
     }